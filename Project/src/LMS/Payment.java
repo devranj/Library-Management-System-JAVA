3b164cb625cc4441e345e4b89ce7b080c573dc85
devranj <devopswithranjeet@gmail.com>
2024-10-15 18:37:12 +0530

    Test Message

---
+++ Project/src/LMS/Payment.java
@@ -0,0 +1,24 @@
+package LMS;
+
+import java.util.Scanner;
+
+public class Payment {
+ public static void main(String[] args){
+
+     Scanner sc = new Scanner(System.in);
+
+     int num = sc.nextInt();
+     System.out.println("Here the first value" + num );
+
+     git commit will help to recognise that something is going to come
+
+             what do you after commit the code ?
+              Push the code to my repo >> Payment Class Added
+
+
+ }
+
+
+
+
+}
